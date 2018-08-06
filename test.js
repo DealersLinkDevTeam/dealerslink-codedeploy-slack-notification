a386e2928d148d8deaaa058c86a2c3a08279cd2e
Jay Reardon <jayreardon@gmail.com>
2018-08-06 15:45:00 -0600

    Adding testing and error capturing

---
+++ test.js
@@ -0,0 +1,42 @@
+const tester = require('./index');
+
+const deployTestData = {
+  Succeeded: true,
+  Failed: false,
+  Skipped: false,
+  InProgress: false,
+  Pending: false
+};
+
+const testData = {
+  eventTriggerName: 'Deploy',
+  createTime: '2018-03-29T17:20:35',
+  completeTime: '2018-03-29T17:20:35',
+  deploymentId: 1,
+  region: 'us-east-1',
+  deploymentGroupName: 'Test-Dev',
+  applicationName: 'Test',
+  status: 'Success',
+  object: 'vehicle',
+  deploymentOverview: JSON.stringify(deployTestData)
+};
+
+const data = JSON.stringify(testData);
+tester.handler(
+  { Records: [
+      {
+        Sns: {
+          Subject: 'Test',
+          Message: data
+        }
+      }
+    ]
+  },
+  null,
+  (res) => {
+    if (res) {
+      console.log(res);
+    }
+  },
+  { }
+);
