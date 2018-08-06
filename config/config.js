3815553a37a771a4a33b05a92c274e3731dbdef8
Jay Reardon <jayreardon@gmail.com>
2018-08-06 14:31:37 -0600

    Updating Deployment information

--- config/config.js
+++ config/config.js
@@ -1,5 +1,4 @@
-/* eslint id-length: off */
 module.exports = {
-  "hookChannel": "notifications",
-  "hookURL": "https://hooks.slack.com/service/..."
+  "hookChannel": "deploy-monitoring",
+  "hookURL": "https://hooks.slack.com/services/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"
 }
